68407bd1dd9c1cacb5ef50834aa0a6517d843495
motazramadan8 <motazramadan198@gmail.com>
2024-12-19 16:37:32 +0200

    Add auth with breez

--- resources/views/theme/login.blade.php
+++ resources/views/theme/login.blade.php
@@ -2,30 +2,39 @@
 @section('title', 'Login')
 
 @section('content')
-    @include('theme.partials.hero', ['title' => 'Login'])
+@include('theme.partials.hero', ['title' => 'Login'])
 
-    <!-- ================ contact section start ================= -->
-    <section class="section-margin--small section-margin">
-        <div class="container">
-            <div class="row">
-                <div class="col-6 mx-auto">
-                    <form action="#/" class="form-contact contact_form" action="contact_process.php" method="post"
-                        id="contactForm" novalidate="novalidate">
-                        <div class="form-group">
-                            <input class="form-control border" name="email" id="email" type="email"
-                                placeholder="Enter email address">
-                        </div>
-                        <div class="form-group">
-                            <input class="form-control border" name="password" id="name" type="password"
-                                placeholder="Enter your password">
-                        </div>
+<!-- ================ contact section start ================= -->
+<section class="section-margin--small section-margin">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 mx-auto">
+                <form action="{{ route('login.store') }}" class="form-contact contact_form" method="post" novalidate="novalidate">
+                    @csrf
+
+                    <!-- Email -->
+                    <div class="form-group">
+                        <input class="form-control border" name="email" type="email"
+                            placeholder="Enter email address" value="{{ old('email') }}">
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="form-group">
+                        <input class="form-control border" name="password" type="password"
+                            placeholder="Enter your password">
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+                    <div class="d-flex justify-content-end align-items-center">
+                        <a href="{{ route('register') }}" class="mr-2">You don't have an account?</a>
                         <div class="form-group text-center text-md-right mt-3">
                             <button type="submit" class="button button--active button-contactForm">Login</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
-    </section>
-    <!-- ================ contact section end ================= -->
+    </div>
+</section>
+<!-- ================ contact section end ================= -->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,6 @@ Route::controller(ThemeController::class)->name('theme.')->group(function () {
     Route::get('/category', 'category')->name('category');
     Route::get('/contact', 'contact')->name('contact');
     Route::get('/blog-details', 'blogDetails')->name('blogDetails');
-    Route::get('/login', 'login')->name('login');
-    Route::get('/register', 'register')->name('register');
 });
 
 Route::get('/dashboard', function () {
